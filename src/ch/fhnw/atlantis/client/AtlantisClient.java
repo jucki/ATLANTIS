92555b50329a4513ffff38c02a371353395a815b
jucki <jonas.stucki@students.fhnw.ch>
2016-11-07 17:27:28 +0100

    commit client interface

--- src/ch/fhnw/atlantis/client/AtlantisClient.java
+++ src/ch/fhnw/atlantis/client/AtlantisClient.java
@@ -3,5 +3,41 @@ package ch.fhnw.atlantis.client;
 /**
  * Created by Jonas on 07.11.2016.
  */
+
+import java.util.List;
+
+import ch.fhnw.atlantis.client.model.Card;
+import ch.fhnw.atlantis.client.model.Figure;
+import ch.fhnw.atlantis.client.model.GameState;
+import ch.fhnw.atlantis.client.model.Tile;
+
+
 public interface AtlantisClient {
+
+
+    public int start();
+
+
+    public List<Tile> getTiles(int playerId);
+    // Returns initially shuffled list of tiles to placed on the board
+
+
+    public List<Card> getDealedCards(int playerId);
+    /**
+     * Returns a List of Cards for one Player
+     * @return
+     */
+
+    public Boolean isMyTurnNow(int playerId);
+    // Returns true if this playersID's turn
+
+    public GameState move(int playerId, Card card, Figure figureToMove);
+    // make a move with playerID, card played and figure to move
+
+    public GameState pay(int playerId, List<Tile> tiles);
+    // pay for water with list of tiles to pay
+
+    public GameState playAnotherCard(int playerId, Card card);
+    // play another card if necessary
+
 }
